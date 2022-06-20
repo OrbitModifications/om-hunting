ce4e39f996b8bb75a982e83ac0eef28d421e1134
Callum Henderson <99361959+OrbitModifications@users.noreply.github.com>
2022-06-20 19:58:08 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games 'gta5'
+
+author 'Orbit Modifications orbitmodifications@gmail.com'
+description 'A complete script for hunting on the QBCore Framework.'
+version '1.0.0'
+
+client_scripts 'client.lua'
+
+server_script 'server.lua'
